3e094e90736838f6fcd7db01b6d6a0840272424d
Nicholas Barosso <nbarosso@gmail.com>
2023-11-12 16:41:07 -0600

    skeleton of event listeners

---
+++ index.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    characterLink.addEventListener('click', () => {
+ 
+    });
+
+    savedLink.addEventListener('click', () => {
+   
+    });
+
+    searchLink.addEventListener('click', () => {
+        
+    });
+
+    landingLink.addEventListener('click', () => {
+  
+    });
+
+});
