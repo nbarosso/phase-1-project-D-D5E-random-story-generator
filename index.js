97b7116dcb64d9116fa7892ea4a3e53ceff6b732
Nicholas Barosso <nbarosso@gmail.com>
2023-11-12 16:42:33 -0600

    functional event listeners and domcontent load

--- index.js
+++ index.js
@@ -12,19 +12,35 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     characterLink.addEventListener('click', () => {
-  
+        landingSection.style.display = 'none';
+        characterSection.style.display = 'block';
+        savedCharacters.style.display = 'none';
+        searchSection.style.display = 'none';
+        footer.style.display = 'none';
     });
 
     savedLink.addEventListener('click', () => {
-      
+        landingSection.style.display = 'none';
+        characterSection.style.display = 'none';
+        savedCharacters.style.display = 'block';
+        searchSection.style.display = 'none';
+        footer.style.display = 'none';
     });
 
     searchLink.addEventListener('click', () => {
-      
+        landingSection.style.display = 'none';
+        characterSection.style.display = 'none';
+        savedCharacters.style.display = 'none';
+        searchSection.style.display = 'block';
+        footer.style.display = 'none';
     });
 
     landingLink.addEventListener('click', () => {
-  
+        landingSection.style.display = 'block';
+        characterSection.style.display = 'none';
+        savedCharacters.style.display = 'none';
+        searchSection.style.display = 'none';
+        footer.style.display = 'block';
     });
 
 });
