55bf1222e4ea52770da0da516f90b669d1714fd2
Nicholas Barosso <nbarosso@gmail.com>
2023-11-12 17:08:17 -0600

    async function to fetch API

--- src/character.js
+++ src/character.js
@@ -6,3 +6,16 @@ const numRandomSkills = Math.floor(Math.random() * 4) + 2
 const maxRandomLanguages = Math.floor(Math.random() * 2) + 1
 const maxRandomProficiencies = 6;
 const maxRandomEquipment = 3;
+
+// Async function to fetch data from the API
+
+async function getRandomData(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return null;
+    }
+}
