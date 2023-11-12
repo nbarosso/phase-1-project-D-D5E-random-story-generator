3daff924727afcf1c51077e528f7cbab430c4f77
Nicholas Barosso <nbarosso@gmail.com>
2023-11-12 17:03:48 -0600

    add async func for delete

--- src/savedcharacter.js
+++ src/savedcharacter.js
@@ -22,3 +22,27 @@ function displaySavedCharacterAsCard(characterData) {
 
     savedCharacterList.appendChild(card);
 }
+
+async function deleteCharacter(characterId) {
+    try {
+        const response = await fetch(`/api/characters/${characterId}`, {
+            method: 'DELETE',
+        });
+
+    } catch (error) {
+        console.error(`Error while deleting character: ${error}`);
+    }
+}
+
+document.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target.classList.contains('deleteCharacterButton')) {
+        const characterId = target.getAttribute('data-id');
+        if (confirm('Are you sure you want to delete this character?')) {
+            deleteCharacter(characterId);
+            target.closest('.character-card').remove();
+        }
+    }
+
+});
