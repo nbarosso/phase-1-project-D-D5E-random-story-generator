cc8b4c948e31a00ed9031c309b70b06849e284e8
Nicholas Barosso <nbarosso@gmail.com>
2023-11-12 16:41:49 -0600

    add document constants

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    const characterLink = document.getElementById('characterLink');
+    const searchLink = document.getElementById('searchLink');
+    const savedLink = document.getElementById('savedLink')
+    const landingSection = document.getElementById('landingSection');
+    const landingLink = document.getElementById('landingLink');
+    const characterSection = document.getElementById('characterSection');
+    const savedCharacters = document.getElementById('savedCharacters');
+    const searchSection = document.getElementById('searchSection');
+    const footer = document.getElementById('footer');
+
 
     characterLink.addEventListener('click', () => {
- 
+  
     });
 
     savedLink.addEventListener('click', () => {
-   
+      
     });
 
     searchLink.addEventListener('click', () => {
-        
+      
     });
 
     landingLink.addEventListener('click', () => {
