b76d7cdb814b69ba0dc3481456150adb6cc800de
Nicholas Barosso <nbarosso@gmail.com>
2023-11-12 17:52:38 -0600

    search build out

--- src/search.js
+++ src/search.js
@@ -1,5 +1,4 @@
 // D&D5E API Search functionality build-out
-
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
 const categorySelect = document.getElementById('categorySelect');
@@ -9,6 +8,37 @@ const selectedInfo = document.getElementById('selectedInfo');
 searchButton.addEventListener('click', performSearch);
 
 async function performSearch() {
+    const searchText = searchInput.value.toLowerCase().trim();
+    const selectedCategory = categorySelect.value;
+
+    // Clear previous results
+    searchResults.innerHTML = '';
+    selectedInfo.innerHTML = '';
+
+    if (searchText.length === 0) {
+        return;
+    }
+
+    try {
+        // Fetch data based on the selected category
+        const response = await fetch(`https://www.dnd5eapi.co/api/${selectedCategory}`);
+        const data = await response.json();
+
+        // Filter data based on search text
+        const filteredData = data.results.filter(item =>
+            item.name.toLowerCase().includes(searchText)
+        );
+
+        // Display search results
+        filteredData.forEach(item => {
+            const listItem = document.createElement('div');
+            listItem.textContent = item.name;
+            listItem.addEventListener('click', () => displaySelectedItem(item.url));
+            searchResults.appendChild(listItem);
+        });
+    } catch (error) {
+        console.error('Error fetching or processing data:', error);
+    }
 }
 
 async function displaySelectedItem(url) {
@@ -16,28 +46,30 @@ async function displaySelectedItem(url) {
         const response = await fetch(`https://www.dnd5eapi.co${url}`);
         const data = await response.json();
 
-        // Create HTML element to show the selected item's info
+        // Create an HTML element to display the selected item's information
         const selectedItemDiv = document.createElement('div');
 
-        // Create a definition list/DL for the key/value pairs
-        const dl = document.createElement('dl');
+        // Create a definition list for the key-value pairs
+        const dl = document.createElement('dl'); //definition list
         
         for (const key in data) {
             if (data.hasOwnProperty(key)) {
                 const value = data[key];
 
-                // Create a definition term/DT for the key
-                const dt = document.createElement('dt');
+                // Create a definition term (dt) for the key
+                const dt = document.createElement('dt'); //definition term
                 dt.textContent = key;
                 dl.appendChild(dt);
 
-                // Create a definition description/DD for the value
-                const dd = document.createElement('dd');
+                // Create a definition description (dd) for the value
+                const dd = document.createElement('dd'); //definition description
 
                 if (Array.isArray(value)) {
                     if (key === "desc") {
+                        // Handle "desc" array property (e.g., spell descriptions)
                         dd.innerHTML = value.map((descItem, index) => `${index + 1}. ${descItem}`).join('<br>');
                     } else if (value.length > 0) {
+                        // Handle other arrays with non-empty values
                         const values = value.map(item => {
                             if (typeof item === 'object') {
                                 return item.name;
@@ -47,7 +79,7 @@ async function displaySelectedItem(url) {
                         dd.textContent = values;
                     }
                 } else if (typeof value === 'object') {
-                    // Handle objects
+                    // Handle nested objects
                     const subProperties = Object.keys(value)
                         .map(subKey => {
                             if (typeof value[subKey] === 'object') {
