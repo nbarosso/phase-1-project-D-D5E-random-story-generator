2e41885410161b1e493f43877e8ac70c4dce2524
Nicholas Barosso <nbarosso@gmail.com>
2023-11-12 17:06:15 -0600

    build generator, add limiter

---
+++ src/character.js
@@ -0,0 +1,8 @@
+// Random hero generator
+
+
+// Limiter on certain attributes to make sense through the game
+const numRandomSkills = Math.floor(Math.random() * 4) + 2
+const maxRandomLanguages = Math.floor(Math.random() * 2) + 1
+const maxRandomProficiencies = 6;
+const maxRandomEquipment = 3;
